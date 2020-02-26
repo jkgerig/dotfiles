aa9ee0a40f3d65254510bd2cadc6e5a0568b73c6
Kris Gerig <kris.gerig@gmail.com>
2020-02-26 14:22:38 -0600

    update install script

--- install.sh
+++ install.sh
@@ -10,11 +10,16 @@
 # NOTE: go ahead and make ${HOME}/.config/nvim without checking
 # to see if neovim is installed because I'm lazy
 
+# NOTE: prompt before most things, I haven't looked at this in years
+# and forget how everything works
+
 cd $HOME
 
-mkdir .oldrc
+if [ ! -d ".oldrc/" ]; then
+    mkdir .oldrc/
+fi
 
-mv -t .oldrc/ \
+mv -i -t .oldrc/ \
     .bash_aliases \
     .bash_profile \
     .bashrc \
@@ -29,12 +34,15 @@ ln -s ${HOME}/.dotfiles/bash_aliases .bash_aliases
 ln -s ${HOME}/.dotfiles/bash_profile .bash_profile
 ln -s ${HOME}/.dotfiles/bashrc .bashrc
 ln -s ${HOME}/.dotfiles/dircolors .dircolors
-ln -s ${HOME}/.dotfiles/git-prompt.sh .git-prompt.sh
 ln -s ${HOME}/.dotfiles/gitconfig .gitconfig
 ln -s ${HOME}/.dotfiles/inputrc .inputrc
 ln -s ${HOME}/.dotfiles/tmux.conf .tmux.conf
 ln -s ${HOME}/.dotfiles/vimrc .vimrc
 
-mv ${HOME}/.config/nvim/init.vim ${HOME}/.oldrc/
-mkdir -p ${HOME}/.config/nvim
+mv -i ${HOME}/.config/nvim/init.vim ${HOME}/.oldrc/
+
+if [ ! -d "${HOME}/.config/nvim" ]; then
+    mkdir ${HOME}/.config/nvim
+fi
+
 ln -s ${HOME}/.dotfiles/init.vim ${HOME}/.config/nvim/init.vim
